a255ae7c72f2d02ac8f14a4fb7349d50f009c66d
kmponis <kmponis@gmail.com>
2019-10-11 01:08:03 +0100

    Change the package import to match folder rename

--- Section_1-Go-Essentials/section2/usegreetings.go
+++ Section_1-Go-Essentials/section2/usegreetings.go
@@ -4,7 +4,7 @@ package main
 import (
 	"fmt"
 	// Original import: "github.com/EngineerKamesh/gofullstack/volume1/section2/greetingspackage"
-	"gofullstack/Section_1-Go-Essentials/section2/greetingspackage"
+	"go-full-stack-app/Section_1-Go-Essentials/section2/greetingspackage"
 )
 
 func main() {
